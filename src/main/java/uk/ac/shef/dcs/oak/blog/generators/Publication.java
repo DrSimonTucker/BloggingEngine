9bff773292cbf2ad0f111607f3a3596d50fd03a8
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-08-10 10:00:58 +0100

    Working on adding publications

---
+++ src/main/java/uk/ac/shef/dcs/oak/blog/generators/Publication.java
@@ -0,0 +1,9 @@
+package uk.ac.shef.dcs.oak.blog.generators;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Publication
+{
+   Map<String, String> fieldList = new TreeMap<String, String>();
+}
---
+++ src/main/java/uk/ac/shef/dcs/oak/blog/generators/Publications.java
@@ -0,0 +1,30 @@
+package uk.ac.shef.dcs.oak.blog.generators;
+
+import java.io.File;
+import java.util.Map;
+
+public class Publications implements Generator
+{
+
+   @Override
+   public String generate(File sourceFile, String[] parameters)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String generate(File sourceFile, String[] parameters, Map<File, String> pageMap)
+   {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public boolean postProcess()
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+}
